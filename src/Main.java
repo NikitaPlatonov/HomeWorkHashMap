bb1466eeb77fa543fac6139dd8a5615143edc69e
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-03-30 16:34:57 +0300

    im tired...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+
+    public static void calculating (String text , Map <Character, Integer> map) {
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isLetter(text.charAt(i))) {
+                map.put(text.charAt(i), + 1);
+            }
+        }
+    }
+
+    public static Integer resultMax (Map <Character, Integer> map) {
+        int max = -1;
+        char letter = '.';
+        for (int i = 0; i < map.size(); i++) {
+            if (map.get(text.charAt(i)) == null) {
+                continue;
+            }
+            int letters = map.get(text.charAt(i));
+            if (letters > max) {
+                max = letters;
+                letter = text.charAt(i);
+            }
+        }
+        return map.get(letter);
+    }
+
+    public static final String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
     public static void main(String[] args) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        calculating(text, map);
+
+        resultMax(map);
+
+
 
     }
 }
